709bc0355c120f30a5a1db46c1721ba9e3c86966
Muhammed Salim <Muhammed.Salim@windstream.com>
2024-04-16 19:22:29 +0530

    initial_commit

---
+++ src/main/java/com/finger_print/Finger_print_spring/FingerPrintSpringApplication.java
@@ -0,0 +1,13 @@
+package com.finger_print.Finger_print_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerPrintSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingerPrintSpringApplication.class, args);
+	}
+
+}
